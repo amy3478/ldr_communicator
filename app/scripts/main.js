08044d1a5656fb7a31b12a7384becc32ea26946c
amy pu <yiming3478@gmail.com>
2016-03-08 16:11:06 -0500

    commited

---
+++ app/scripts/main.js
@@ -0,0 +1,144 @@
+/*!
+ *
+ *  Web Starter Kit
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+/* eslint-env browser */
+(function() {
+  'use strict';
+
+  // Check to make sure service workers are supported in the current browser,
+  // and that the current page is accessed from a secure origin. Using a
+  // service worker from an insecure origin will trigger JS console errors. See
+  // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
+  var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+    );
+
+  if ('serviceWorker' in navigator &&
+      (window.location.protocol === 'https:' || isLocalhost)) {
+    navigator.serviceWorker.register('service-worker.js')
+    .then(function(registration) {
+      // Check to see if there's an updated version of service-worker.js with
+      // new files to cache:
+      // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-registration-update-method
+      if (typeof registration.update === 'function') {
+        registration.update();
+      }
+
+      // updatefound is fired if service-worker.js changes.
+      registration.onupdatefound = function() {
+        // updatefound is also fired the very first time the SW is installed,
+        // and there's no need to prompt for a reload at that point.
+        // So check here to see if the page is already controlled,
+        // i.e. whether there's an existing service worker.
+        if (navigator.serviceWorker.controller) {
+          // The updatefound event implies that registration.installing is set:
+          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+          var installingWorker = registration.installing;
+
+          installingWorker.onstatechange = function() {
+            switch (installingWorker.state) {
+              case 'installed':
+                // At this point, the old content will have been purged and the
+                // fresh content will have been added to the cache.
+                // It's the perfect time to display a "New content is
+                // available; please refresh." message in the page's interface.
+                break;
+
+              case 'redundant':
+                throw new Error('The installing ' +
+                                'service worker became redundant.');
+
+              default:
+                // Ignore
+            }
+          };
+        }
+      };
+    }).catch(function(e) {
+      console.error('Error during service worker registration:', e);
+    });
+  }
+
+  // Your custom JavaScript goes here
+})();
+
+
+
+$(document).ready(function() {
+
+  function check24(hour) {
+    return (hour >= 24) ? hour - 24 : hour;
+}
+// Create two variable with the names of the months and days in an array
+var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+// Create a newDate() object
+var newDate = new Date();
+// Extract the current date from Date object
+newDate.setDate(newDate.getDate());
+// Output the day, date, month and year   
+$('#date1').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+
+setInterval( function() {
+    // Create a newDate() object and extract the seconds of the current time on the visitor's
+    var seconds = new Date().getSeconds();
+    // Add a leading zero to seconds value
+    $(".sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+    },1000);
+    
+setInterval( function() {
+    // Create a newDate() object and extract the minutes of the current time on the visitor's
+    var minutes = new Date().getMinutes();
+    // Add a leading zero to the minutes value
+    $(".min").html(( minutes < 10 ? "0" : "" ) + minutes);
+    },1000);
+    
+setInterval( function() {
+    // Create a newDate() object and extract the hours of the current time on the visitor's
+    var hours = new Date().getHours();
+    // Add a leading zero to the hours value
+    $("#hours1").html(( hours < 10 ? "0" : "" ) + hours);
+    }, 1000);  
+
+var def = newDate.getTimezoneOffset()/60;
+var gmt = (newDate.getHours() + def);
+var sg =check24(((gmt + 8) > 24) ? ((gmt + 8) - 24) : (gmt + 8)); 
+var sgDate = new Date();
+sgDate.setTime(newDate.getTime() + newDate.getTimezoneOffset() * 60000 + 480 * 60000);
+console.log(newDate.getTime());
+$('#date2').html(dayNames[sgDate.getDay()] + " " + sgDate.getDate() + ' ' + monthNames[sgDate.getMonth()] + ' ' + sgDate.getFullYear());
+
+setInterval( function() {
+    // Create a newDate() object and extract the hours of the current time on the visitor's
+    // Add a leading zero to the hours value
+    var hours = sgDate.getHours();
+    $("#hours2").html(( hours < 10 ? "0" : "" ) + hours);
+    }, 1000);  
+
+
+$("#btn-light").click(function() {
+  $(this).toggleClass('on');
+});
+});
+
